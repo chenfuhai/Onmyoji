a47a549dd101e81fd27a461c5afa9f2bc2293919
chenfuhai <1169555653@qq.com>
2017-06-05 10:22:09 +0800

    添加了查询的类

--- src/action/Test.java
+++ src/action/Test.java
@@ -1,7 +1,11 @@
 package action;
 
+import utils.Query;
+
 import org.apache.struts2.ServletActionContext;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.opensymphony.xwork2.ActionContext;
 import com.opensymphony.xwork2.ActionSupport;
 
@@ -13,8 +17,13 @@ public class Test extends ActionSupport{
 	public String execute() throws Exception {
 		// TODO Auto-generated method stub
 		String msg = NetUtils.readString(ServletActionContext.getRequest().getInputStream());
+		Gson gson = new GsonBuilder().serializeNulls().create();
+		Query query = gson.fromJson(msg, Query.class);
+		
+		
 		ServletActionContext.getResponse().getWriter().println(msg);
-		System.out.println(2134);
+		System.out.println(234234);
+		System.out.println(query.toString());
 		return null;
 	}
 	
